e0cc545c3a3be8015eb0874060d12d84352e9afe
NicolasCeronM <106988078+NicolasCeronM@users.noreply.github.com>
2023-09-03 14:20:14 -0300

    Iniciando pagina de login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { LoginPageRoutingModule } from './login-routing.module';
-
 import { LoginPage } from './login.page';
-import {Component} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {ThemePalette} from '@angular/material/core';
+import {NgFor} from '@angular/common';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule,FormsModule, MatFormFieldModule, MatInputModule
+    LoginPageRoutingModule,
+    FormsModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatTooltipModule, 
+    MatIconModule,
+    MatCheckboxModule, 
+    NgFor, 
+    FormsModule
   ],
   declarations: [LoginPage]
 })
