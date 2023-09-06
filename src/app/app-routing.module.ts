65a11706d4081f6f7590d678e71084ba7b15fd7a
Benjamin barraza <ben.barraza@duocuc.cl>
2023-09-06 00:34:41 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	new file:   src/app/olvide/olvide-routing.module.ts 	new file:   src/app/olvide/olvide.module.ts 	new file:   src/app/olvide/olvide.page.html 	new file:   src/app/olvide/olvide.page.scss 	new file:   src/app/olvide/olvide.page.spec.ts 	new file:   src/app/olvide/olvide.page.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'olvide',
+    loadChildren: () => import('./olvide/olvide.module').then( m => m.OlvidePageModule)
   },
+
 ];
 
 @NgModule({
