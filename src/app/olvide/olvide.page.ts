015f89eca7d267368c318d9f2353aeca05e4bbaa
Benjamin barraza <ben.barraza@duocuc.cl>
2023-09-06 11:45:56 -0300

    modified:   src/app/olvide/olvide.page.html 	modified:   src/app/olvide/olvide.page.ts

--- src/app/olvide/olvide.page.ts
+++ src/app/olvide/olvide.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-olvide',
   templateUrl: './olvide.page.html',
   styleUrls: ['./olvide.page.scss'],
 })
-export class OlvidePage implements OnInit {
+export class OlvidePage {
 
-  constructor() { }
-
-  ngOnInit() {
+   user = {
+    username: "",
+    password: ""
   }
+  
+  
+  constructor(private router: Router) {}
 
+  IrAlHome(){
+    let navegationExtras: NavigationExtras = {
+      state: {
+        user: this.user
+      }
+    }
+      this.router.navigate(['/login'], navegationExtras)
+    }
+  customCounterFormatter(inputLength: number, maxLength: number) {
+    return `${maxLength - inputLength} characters remaining`;
+  }
 }
