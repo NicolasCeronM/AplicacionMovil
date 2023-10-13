9791222bc7fcc6eacdb37c4312287c886f176406
NicolasCeronM <106988078+NicolasCeronM@users.noreply.github.com>
2023-10-12 22:56:02 -0300

    Incorporacion de API

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { ApiService } from '../service/api.service';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -10,33 +9,62 @@ import { ApiService } from '../service/api.service';
 export class LoginPage {
   hide = true;
 
+  recuerdame = false
+
   user = {
     username: "",
     password: ""
   }
 
+  usuarios: any
+
   constructor(private router: Router, private api: ApiService) { }
+
+  ngOnInit() {
+
+    //Se carga el contenido de la api a la variable
+
+    this.api.get().subscribe(res => {
+      this.usuarios = res
+      console.log(this.usuarios)
+
+    })
+
+
+  }
 
 
   IrAlHome() {
-    let navegationExtras: NavigationExtras = {
-      state: {
-        user: this.user
+
+    //todo| Validamos el inicio de sesion con datos de la API
+
+    for (let i = 0; i < this.usuarios.length; i++) { //Recorre la lista de usuarios
+      const item = this.usuarios[i]
+
+      if (this.user.username == item.nombre_usuario) { //Vlida el ingeso de usuario
+
+        if (this.recuerdame) {
+
+          localStorage.setItem('username', this.user.username)
+
+        }
+
+        let navegationExtras: NavigationExtras = {
+          state: {
+            user: this.user
+          }
+        }
+
+        this.router.navigate(['/home'], navegationExtras)
+      } else {
+        console.log('NO PAPITO')
       }
     }
-    this.router.navigate(['/home'], navegationExtras)
   }
 
-  cargarApi(){
 
-    this.api.get().subscribe((contenido) =>{
-      console.log(contenido)
-    },
-    (error) =>{
-      console.log(error)
-    })
 
-  }
 
 
 }
+
